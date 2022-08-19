f9615a4ba78efa89907d0b08228c1958f9a4d8e1
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2022-08-19 09:06:58 +0300

    Update

--- app/src/main/java/com/example/food_app/DetailFragment.kt
+++ app/src/main/java/com/example/food_app/DetailFragment.kt
@@ -21,7 +21,9 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
     }
+
 
     fun imageAdd(view: View){
 
